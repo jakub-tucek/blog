7e765ed2541dc4d603a530a35826b6f2b10c743e
Jakub Tucek <jakub.tucek@gmail.com>
2022-01-02 23:42:30 +0100

    Update gh pages branch

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ npm run build
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch --all
 git checkout gh-pages
-mv public/* .
+cat "node_modules" > .gitignore
 git commit -m "Deploy"
 git push
 git checkout master
