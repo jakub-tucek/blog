c5ba13d552db953156fc672d2b4b83b9727ca72c
Jakub Tucek <jakub.tucek@gmail.com>
2022-01-03 19:29:19 +0100

    Update to blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,52 +1,133 @@
-if (process.env.BUILD_ENV) {
-  console.log(`Loading .env.${process.env.BUILD_ENV}`);
-  require('dotenv').config({
-    path: `.env.${process.env.BUILD_ENV}`,
-  });
-} else {
-  console.log('Loading .env');
-  require('dotenv').config({
-    path: `.env`,
-  });
-}
-
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://jakubtucek.cz',
-    title: 'jakub-tucek',
+    title: `The Blog`,
+    author: {
+      name: `Jakub Tucek`,
+      summary: `who lives and works in Prague building useful things.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    social: {
+      twitter: `nekonecno8`,
+    },
   },
   plugins: [
     'gatsby-plugin-typescript',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-image',
+    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        trackingIds: [process.env.GATSBY_GA_TRACKING_ID],
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
       },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-scroll-reveal',
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        icon: 'src/svg/start-laptop.svg',
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-transformer-remark`,
       options: {
-        name: 'images',
-        path: './src/images/',
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
       },
-      __key: 'images',
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `ADD YOUR TRACKING ID HERE`,
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Gatsby Starter Blog RSS Feed",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
-};
+}
--- src/components/layout/PageHeader.tsx
+++
@@ -1,6 +0,0 @@
-import type { FC } from 'react';
-import React from 'react';
-
-export const PageHeader: FC = () => {
-  return <>Div</>;
-};
